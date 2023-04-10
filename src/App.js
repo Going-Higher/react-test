e3bdbaeaf17dd295730006976b62b892b5749ff0
Going-Higher <djosy1207@naver.com>
2023-04-10 21:40:37 +0900

    다양한 증감

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import React, { useState } from "react";
+
 function App() {
-  return  <div>안녕</div>;
+  const [num, setNum] = useState(0);
+
+  const onBtnIncreaseClicked = () => {
+    setNum(num + 1);
+  };
+
+  // const onBtnIncrease10Clicked = () => {
+  //   setNum(num + 10);
+  // };
+
+  const onBtnDecreaseClicked = () => {
+    setNum(num - 1);
+  };
+
+  // const onBtnDecrease10Clicked = () => {
+  //   setNum(num - 10);
+  // };
+
+  return (
+    <>
+      NUM : {num}
+      <br/>
+      <button onClick={onBtnIncreaseClicked}>+1</button>
+      <br/>
+      <button onClick={() => { setNum(num + 10) }}>+10</button>
+      <br/>
+      <button onClick={onBtnDecreaseClicked}>-1</button>
+      <br/>
+      <button onClick={() => { setNum(num -10) }}>-10</button>
+    </>
+    
+  );  
 }
 
 export default App;
